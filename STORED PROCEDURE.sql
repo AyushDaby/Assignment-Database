8c169ee2289ecfdacb6875858fc2c9ecf9a217d0
Amisha2302 <ooturamisha@gmail.com>
2025-03-13 12:51:27 +0000

    STORED PROCEDURE.sql

--- STORED PROCEDURE.sql
+++ STORED PROCEDURE.sql
@@ -35,3 +35,52 @@ END;
 
 
 EXEC sp_disp_record @pet_id = 'P001', @record_id = 'RC0001';
+
+
+---CREATE TRIGGER FOR INSERT SUPPLYEQUIPMENT
+CREATE TRIGGER tg_insert_supplyEquipment
+ON supplyEquipment
+INSTEAD OF INSERT
+AS
+BEGIN
+    DECLARE @supply_id supIdDom,
+            @sdescription VARCHAR(40),
+            @Barcode VARCHAR(50),
+            @quantity INT,
+            @Price DECIMAL(5,2),
+            @category VARCHAR(40);
+
+    -- Retrieve values from the INSERTED table
+    SELECT 
+        @supply_id = supply_id,
+        @sdescription = sdescription,
+        @Barcode = Barcode,
+        @quantity = quantity,
+        @Price = Price,
+        @category = category
+    FROM INSERTED;
+
+    -- Check if supply_id is NULL
+    IF (@supply_id IS NULL)
+    BEGIN
+        PRINT 'Please enter the supply ID.';
+        RETURN; -- Exit the trigger if supply_id is NULL
+    END;
+
+    -- Check if quantity is greater than 0
+    IF (@quantity <= 0)
+    BEGIN
+        PRINT 'Quantity must be greater than 0.';
+        RETURN; -- Exit the trigger if quantity is invalid
+    END;
+
+    -- Perform the actual insert
+    INSERT INTO supplyEquipment (supply_id, sdescription, Barcode, quantity, Price, category)
+    SELECT 
+        supply_id, sdescription, Barcode, quantity, Price, category
+    FROM INSERTED;
+
+    PRINT 'A new record has been successfully inserted into the supplyEquipment table.';
+END;
+
+
